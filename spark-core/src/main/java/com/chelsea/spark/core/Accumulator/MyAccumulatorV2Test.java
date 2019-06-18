bd49a0aa6ec7180568d1b3e6ff40df059b89a338
baojun_a <baojun_a@aspirecn.com>
2019-06-18 15:30:53 +0800

    修改累加器

--- spark-core/src/main/java/com/chelsea/spark/core/Accumulator/MyAccumulatorV2Test.java
+++ spark-core/src/main/java/com/chelsea/spark/core/Accumulator/MyAccumulatorV2Test.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
 
 /**
@@ -23,20 +24,26 @@ public class MyAccumulatorV2Test {
         JavaSparkContext sc = new JavaSparkContext(conf);
         List<String> list = Arrays.asList("A", "B", "A", "D", "E", "D", "G", "H", "I", "A", "B", "I", "G", "D", "I");
         // 有3个分区，累加器就执行3次merge方法
-        JavaRDD<String> cacheRdd = sc.parallelize(list, 3).cache();
+        JavaRDD<String> cacheRdd = sc.parallelize(list, 3);
         MyAccumulatorV2 myAccumulatorV2 = new MyAccumulatorV2();
         // 需要注册，不然在运行过程中，会抛出一个序列化异常
         sc.sc().register(myAccumulatorV2);
-        cacheRdd.foreach(new VoidFunction<String>() {
+        JavaRDD<String> map = cacheRdd.map(new Function<String, String>() {
 
             private static final long serialVersionUID = 1L;
 
             @Override
-            public void call(String t) throws Exception {
+            public String call(String t) throws Exception {
                 myAccumulatorV2.add(t);
+                return t;
             }
-
+            
         });
+        // 缓存rdd，防止后续action算子多次触发transformation算子，导致累加器重复计算
+        map.cache();
+        map.count();
+        System.out.println(myAccumulatorV2.value());
+        map.count();
         System.out.println(myAccumulatorV2.value());
         sc.close();
     }
