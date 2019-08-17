f442df353ddfdb4ad837c0c69a16173410ea2840
baojun_a <baojun_a@aspirecn.com>
2019-08-17 16:36:32 +0800

    修改spark接收kafka数据

--- spark-stream/src/main/java/com/chelsea/spark/stream/SparkStreamKafka.java
+++ spark-stream/src/main/java/com/chelsea/spark/stream/SparkStreamKafka.java
@@ -1,21 +1,23 @@
 package com.chelsea.spark.stream;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import kafka.serializer.StringDecoder;
-
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaPairInputDStream;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka.KafkaUtils;
-
-import scala.Tuple2;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
 
 /**
  * spark流获取kafka消息
@@ -25,50 +27,45 @@ import scala.Tuple2;
  */
 public class SparkStreamKafka {
 
-    private static final String brokers = "172.18.20.237:9092";
+    private static final String brokers = "127.0.0.1:9092";
     private static final String kafkaTopics = "sparkTestLogTopic";
     private static final String groupId = "sparkTestLogGroup";
 
     public static void main(String[] args) throws Exception {
         // 构建Spark Streaming上下文
-        SparkConf conf = new SparkConf().setMaster("local").setAppName("SparkStreamKafka");
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("SparkStreamKafka");
 
         JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
         jssc.sparkContext().setLogLevel("error");
-        jssc.checkpoint("/home/dev/tmp/checkpoint");
+        Set<String> topicsSet = new HashSet<>(Arrays.asList(kafkaTopics.split(",")));
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
-        Map<String, String> kafkaParams = new HashMap<String, String>();
-        kafkaParams.put("metadata.broker.list", brokers);
-        kafkaParams.put("group.id", groupId);
+        // Create direct kafka stream with brokers and topics
+        JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(
+            jssc,
+            LocationStrategies.PreferConsistent(),
+            ConsumerStrategies.Subscribe(topicsSet, kafkaParams));
 
-        // 构建topic set
-        String[] kafkaTopicsSplited = kafkaTopics.split(",");
-
-        Set<String> topics = new HashSet<String>();
-        for (String kafkaTopic : kafkaTopicsSplited) {
-            topics.add(kafkaTopic);
-        }
-        // spark stream自己维护kafka消息偏移量，不会同步偏移量给zookeeper
-        JavaPairInputDStream<String, String> inputDStream =
-                KafkaUtils.createDirectStream(jssc, String.class, String.class, StringDecoder.class,
-                        StringDecoder.class, kafkaParams, topics);
-        inputDStream.foreachRDD(new VoidFunction<JavaPairRDD<String, String>>() {
+        // Get the lines, split them into words, count the words and print
+        JavaDStream<String> lines = messages.map(new Function<ConsumerRecord<String, String>, String>() {
 
             private static final long serialVersionUID = 1L;
 
             @Override
-            public void call(JavaPairRDD<String, String> rdd) throws Exception {
-                rdd.foreach(new VoidFunction<Tuple2<String, String>>() {
-
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    public void call(Tuple2<String, String> t) throws Exception {
-                        System.out.println("---------------------->" + t._1 + "=" + t._2);
-                    }
-                });
+            public String call(ConsumerRecord<String, String> v1) throws Exception {
+                System.out.println("接收消息topic：" + v1.topic() + "，key：" + v1.key() + "，value：" + v1.value());
+                return v1.value();
             }
+            
         });
+        
+        lines.print();
+
+        // Start the computation
         jssc.start();
         jssc.awaitTermination();
     }
